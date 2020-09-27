afa4fbf76a0635e6705514aff4d18dbd66e90147
Baptiste Lombard <btlcasio@gmail.com>
2020-09-27 16:53:23 +0200

    console log

--- dist/index.js
+++ dist/index.js
@@ -13,18 +13,14 @@ const run = async () => {
     const token = core.getInput('token');
     const octokit = github.getOctokit(token);
 
-    console.log({
-      owner: github.context.payload.repository.full_name.split('/')[0],
-      repo: github.context.payload.repository.name,
-    });
-
     const { data } = await octokit.pulls.list({
       owner: github.context.payload.repository.full_name.split('/')[0],
       repo: github.context.payload.repository.name,
       state: 'open',
     });
 
-    console.log(data);
+    console.log(data.assignees);
+    console.log(data.labels);
     // console.log(octokit);
     // console.log(core);
   } catch (error) {
