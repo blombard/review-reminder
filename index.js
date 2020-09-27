b8bda5d4bc7956f02ff6b8ec1131c25810db2f5c
Baptiste Lombard <btlcasio@gmail.com>
2020-09-27 17:18:29 +0200

    send a comment

--- index.js
+++ index.js
@@ -5,17 +5,26 @@ const run = async () => {
   try {
     const token = core.getInput('token');
     const octokit = github.getOctokit(token);
+    // const owner = github.context.payload.repository.full_name.split('/')[0];
+    const owner = github.context.payload.sender.login;
+    const repo = github.context.payload.repository.name;
+    const pull_number = context.payload.pull_request.number;
 
     const { data } = await octokit.pulls.list({
-      owner: github.context.payload.repository.full_name.split('/')[0],
-      repo: github.context.payload.repository.name,
+      owner,
+      repo,
       state: 'open',
     });
 
-    console.log(data[0].assignees);
-    console.log(data[0].labels);
-    // console.log(octokit);
-    // console.log(core);
+    console.log(data[0]);
+    // console.log(data[0].labels);
+
+    await octokit.pulls.createComment({ 
+      owner,
+      repo,
+      pull_number,
+      body: "Hello, world! Thanks for creating the PR",
+    })
   } catch (error) {
     core.setFailed(error.message);
   }
